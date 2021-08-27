9d70771ca8cab5bef8d2d862982e47f2b0dc2da4
KilRol <sniper.lech@gmail.com>
2021-08-28 00:10:12 +0300

    Updates

--- mail.php
+++ mail.php
@@ -1,12 +1,54 @@
+<!-- <?php
+        header("Access-Control-Allow-Origin: *");
+        $recepient = "sniper.lech@yandex.ru";
+        $sitename = "Название сайта";
+
+        $name = trim($_POST["name"]);
+        $phone = trim($_POST["phone"]);
+        $text = trim($_POST["text"]);
+        $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+
+        $pagetitle = "Новая заявка с сайта \"$sitename\"";
+        mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient"); ?>-->
+
 <?php
-header("Access-Control-Allow-Origin: *");
-$recepient = "sniper.lech@yandex.ru";
-$sitename = "Название сайта";
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+//Load Composer's autoloader
+require('PHPMailer/Exception.php');
+require('PHPMailer/SMTP.php');
+require('PHPMailer/PHPMailer.php');
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.yandex.ru';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'sniper.lech@yandex.ru';                     //SMTP username
+    $mail->Password   = '08072000';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+ 
+    //Recipients
+    $mail->setFrom('sniper.lech@yandex.ru', 'Mailer');
+    $mail->addAddress('sniper.lech@yandex.ru', 'Joe User');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
